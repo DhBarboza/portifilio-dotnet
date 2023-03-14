2b8b8cacf00254777b7dc06b2c5426f6d91362a5
Douglas Henrique Teixeira Barboza <dev.dhbarboza@gmail.com>
2023-03-13 22:00:02 -0300

    init project

---
+++ Portifolio.API/Portifolio.API/Program.cs
@@ -0,0 +1,33 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+
+app.UseHttpsRedirection();
+
+
+
+app.MapPost("/", (Contact contact) =>
+{
+   
+})
+
+.WithOpenApi();
+
+app.Run();
+
+public class Contact 
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string Subject { get; set; }
+    public string Message { get; set; }
+    public DateTime Date { get; set; }
+}
